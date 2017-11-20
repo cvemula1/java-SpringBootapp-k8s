13680495dd01f10f64be78fe6404a218e42a6955
cvemula1 <cvemula4511@ucumberlands.edu>
2017-11-19 22:07:48 -0600

    Update dockerbuild.sh

--- dockerbuild.sh
+++ dockerbuild.sh
@@ -5,3 +5,4 @@ docker build -t springboot .
 docker tag springboot:latest $DOCKER_ID_USER/$DOCKER_IMAGE:latest
 docker login -u $DOCKER_ID_USER -p $DOCKER_PW - hub.docker.com
 docker push $DOCKER_ID_USER/$DOCKER_IMAGE:latest
+docker images -qa | xargs docker rmi -f
